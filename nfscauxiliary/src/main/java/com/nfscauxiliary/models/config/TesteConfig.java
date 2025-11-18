7bef49b4b6831b968355a6edaf98a7f1d562fc2e
rodrigo <rodrigoerojas30@gmail.com>
2025-11-17 21:16:35 -0300

    create database and fix address class

--- nfscauxiliary/src/main/java/com/nfscauxiliary/models/config/TesteConfig.java
+++ nfscauxiliary/src/main/java/com/nfscauxiliary/models/config/TesteConfig.java
@@ -5,8 +5,9 @@ import com.nfscauxiliary.models.entities.Address;
 public class TesteConfig {
     public static void main(String[] args) {
 
-        System.out.println("Teste de configuraçao executed");
-        Address address = new Address(1, "123 Main St", "Springfield", "IL", "62701", "USA");
+        System.out.println("Teste de config executed");
+        Address address = new Address(1, "123 Main St","Chesterfield", "198",
+                                         "Springfield", "IL", "NY", "87393-192");
         System.out.println(address);
     }
 }
--- nfscauxiliary/src/main/java/com/nfscauxiliary/models/entities/Address.java
+++ nfscauxiliary/src/main/java/com/nfscauxiliary/models/entities/Address.java
@@ -6,14 +6,19 @@ import java.util.*;
 public class Address {
     private Integer idAddress;
     private String street;
+    private String district;
+    private String residence_number;
     private String city;
     private String state;
     private String uf;
     private String zipCode;
 
-    public Address(Integer idAddress, String street, String city, String state, String uf, String zipCode) {
+    public Address(Integer idAddress, String street, String district, String residence_number, 
+                    String city, String state, String uf, String zipCode) {
         this.idAddress = idAddress;
         this.street = street;
+        this.residence_number = residence_number;
+        this.district = district;
         this.city = city;
         this.state = state;
         this.uf = uf;
@@ -22,8 +27,11 @@ public class Address {
 
     @Override
     public String toString() {
-        return "Address [idAddress=" + idAddress + ", street=" + street + ", city=" + city + ", state=" + state
-                + ", uf=" + uf + ", zipCode=" + zipCode + "]";
+        return "Address [idAddress=" + idAddress + ", street=" + street + ", district=" + district
+                + ", residence_number=" + residence_number + ", city=" + city + ", state=" + state + ", uf=" + uf
+                + ", zipCode=" + zipCode + "]";
     }
+
+    
 
 }
